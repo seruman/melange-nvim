1c3ce5378be1e4507549e60f11411d609959d16c
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-08-16 23:22:36 +0300

    palettes/light: revert pre 91c6e23 colors

--- lua/melange/palettes/light.lua
+++ lua/melange/palettes/light.lua
@@ -1,35 +1,42 @@
 -- stylua: ignore
+
+-- TODO(selman): With commit 91c6e239ffd23a373c3e37a8080fa2da983d71c4
+-- the color palette has been changed, which I quite frankly don't like.
+-- Below is a the old color palette mushed together with the new color names;
+-- overbg and faded removed, float and ui added, see the commit for more info.
+-- https://github.com/savq/melange-nvim/commit/91c6e239ffd23a373c3e37a8080fa2da983d71c4
+
 return {
-  a = {
-    bg      = "#F1F1F1",
-    float   = "#E9E1DB",
-    sel     = "#D9D3CE",
-    ui      = "#A98A78",
-    com     = "#7D6658",
-    fg      = "#54433A",
-  },
-  b = {
-    red     = "#BF0021",
-    yellow  = "#A06D00",
-    green   = "#3A684A",
-    cyan    = "#3D6568",
-    blue    = "#465AA4",
-    magenta = "#904180",
-  },
-  c = {
-    red     = "#C77B8B",
-    yellow  = "#BC5C00",
-    green   = "#6E9B72",
-    cyan    = "#739797",
-    blue    = "#7892BD",
-    magenta = "#BE79BB",
-  },
-  d = {
-    red     = "#F1DEDF",
-    yellow  = "#CCA478",
-    green   = "#D0E9D1",
-    cyan    = "#CDE8E7",
-    blue    = "#E0E2E8",
-    magenta = "#E8E0E8",
-  },
+    a = {
+        bg    = "#F4F0ED",
+        float = "#E9E1DB",
+        sel   = "#DED2C8",
+        com   = "#A38D78",
+        ui    = "#867462",
+        fg    = "#6B5C4D",
+    },
+    b = {
+        red     = "#C65333",
+        yellow  = "#C29830",
+        green   = "#659E69",
+        cyan    = "#436460",
+        blue    = "#485F84",
+        magenta = "#854882",
+    },
+    c = {
+        red     = "#D7898C",
+        yellow  = "#CC7F2B",
+        green   = "#83B887",
+        cyan    = "#729893",
+        blue    = "#ABB9D6",
+        magenta = "#BE79BB",
+    },
+    d = {
+        red     = "#E0BFBB",
+        yellow  = "#E2C28C",
+        green   = "#C6ECC8",
+        cyan    = "#C2EAE9",
+        blue    = "#DFE3EB",
+        magenta = "#EBDFEA",
+    },
 }
