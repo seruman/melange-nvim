3a040163920152156532a100c032c94c45d256e0
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-08-16 23:37:42 +0300

    palettes/light: format

--- lua/melange/palettes/light.lua
+++ lua/melange/palettes/light.lua
@@ -7,36 +7,36 @@
 -- https://github.com/savq/melange-nvim/commit/91c6e239ffd23a373c3e37a8080fa2da983d71c4
 
 return {
-    a = {
-        bg    = "#F4F0ED",
-        float = "#E9E1DB",
-        sel   = "#DED2C8",
-        com   = "#A38D78",
-        ui    = "#867462",
-        fg    = "#6B5C4D",
-    },
-    b = {
-        red     = "#C65333",
-        yellow  = "#C29830",
-        green   = "#659E69",
-        cyan    = "#436460",
-        blue    = "#485F84",
-        magenta = "#854882",
-    },
-    c = {
-        red     = "#D7898C",
-        yellow  = "#CC7F2B",
-        green   = "#83B887",
-        cyan    = "#729893",
-        blue    = "#ABB9D6",
-        magenta = "#BE79BB",
-    },
-    d = {
-        red     = "#E0BFBB",
-        yellow  = "#E2C28C",
-        green   = "#C6ECC8",
-        cyan    = "#C2EAE9",
-        blue    = "#DFE3EB",
-        magenta = "#EBDFEA",
-    },
+  a = {
+    bg = '#F4F0ED',
+    float = '#E9E1DB',
+    sel = '#DED2C8',
+    com = '#A38D78',
+    ui = '#867462',
+    fg = '#6B5C4D',
+  },
+  b = {
+    red = '#C65333',
+    yellow = '#C29830',
+    green = '#659E69',
+    cyan = '#436460',
+    blue = '#485F84',
+    magenta = '#854882',
+  },
+  c = {
+    red = '#D7898C',
+    yellow = '#CC7F2B',
+    green = '#83B887',
+    cyan = '#729893',
+    blue = '#ABB9D6',
+    magenta = '#BE79BB',
+  },
+  d = {
+    red = '#E0BFBB',
+    yellow = '#E2C28C',
+    green = '#C6ECC8',
+    cyan = '#C2EAE9',
+    blue = '#DFE3EB',
+    magenta = '#EBDFEA',
+  },
 }
