70d339f2e2b65861ebf27e677e8cbbe1397dacf5
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-03-19 23:22:00 +0300

    lualine: add lualine theme

---
+++ lua/lualine/themes/melange.lua
@@ -0,0 +1,52 @@
+-- NOTE(selman): Copied from;
+-- https://github.com/Martin1887/melangeDarkerWarm-nvim/blob/master/lua/lualine/themes/melange.lua
+
+local bg = vim.opt.background:get()
+
+-- package.loaded['melange/palettes/' .. bg] = nil -- Only needed for development
+local palette = require('melange/palettes/' .. bg)
+
+local a = palette.a -- Grays
+local b = palette.b -- Bright foreground colors
+local c = palette.c -- Foreground colors
+local d = palette.d -- Background colors
+
+local insert_blue = '#9999BB'
+
+return {
+  normal = {
+    a = { bg = a.com, fg = a.bg },
+    b = { bg = a.bg, fg = a.com },
+    c = { bg = a.float, fg = a.com },
+  },
+  insert = {
+    a = { bg = insert_blue, fg = a.bg },
+    b = { bg = a.bg, fg = insert_blue },
+    c = { bg = a.float, fg = insert_blue },
+  },
+  command = {
+    a = { bg = c.yellow, fg = a.bg },
+    b = { bg = a.bg, fg = c.yellow },
+    c = { bg = a.float, fg = c.yellow },
+  },
+  visual = {
+    a = { bg = c.magenta, fg = a.bg },
+    b = { bg = a.bg, fg = c.magenta },
+    c = { bg = a.float, fg = c.magenta },
+  },
+  replace = {
+    a = { bg = c.green, fg = a.bg },
+    b = { bg = a.bg, fg = c.green },
+    c = { bg = a.float, fg = c.green },
+  },
+  terminal = {
+    a = { bg = c.yellow, fg = a.bg },
+    b = { bg = a.bg, fg = c.yellow },
+    c = { bg = a.float, fg = c.yellow },
+  },
+  inactive = {
+    a = { bg = a.com, fg = a.bg },
+    b = { bg = a.bg, fg = a.com, gui = 'bold' },
+    c = { bg = a.float, fg = a.com },
+  },
+}
